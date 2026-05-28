5acc7bf26a1d29b801059c61cf7cf0df69312e58
shashank <shashank99928@gmail.com>
2026-05-28 22:04:23 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken'
 import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from './prisma'
 
+
+
 export const validateRoute = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const token = req.cookies.TRAX_ACCESS_TOKEN
